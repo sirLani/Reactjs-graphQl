9f3df44568e8962062ad331cd76886681a000679
Olaide <laide.tech@rebasive.com>
2021-07-19 13:20:25 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
+import { PokemonsContainer } from "./containers/pokemonContainer";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon2.vercel.app/",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+      </ApolloHooksProvider>
+</ApolloProvider>
   );
 }
 
---
+++ src/containers/pokemonContainer.js
@@ -0,0 +1,20 @@
+﻿import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Pokemon } from "../components/pokemon";
+import { GET_POKEMONS } from "../graphql/get-pokemons";
+
+export function PokemonsContainer() {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+
+  return (
+    <div className="container">
+      {pokemons &&
+        pokemons.map((pokemon) => (
+          
+          <Pokemon key={pokemon.id} pokemon={pokemon} />
+        ))}
+    </div>
+  );
+}
